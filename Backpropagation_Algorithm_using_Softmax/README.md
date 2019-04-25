0db29adb34ac88f11b8b6770a78868116668d585
adeveloperdiary <adeveloperdiary@gmail.com>
2019-04-24 21:40:15 -0400

    softmax

---
+++ Backpropagation_Algorithm_using_Softmax/README.md
@@ -0,0 +1,9 @@
+This is the code for the following tutorial.
+
+
+
+Using just one external library to load the MNIST data. Run the following command to install it
+
+```
+pip install python-mnist
+```
---
+++ Backpropagation_Algorithm_using_Softmax/main.py
@@ -0,0 +1,149 @@
+import numpy as np
+import datasets.mnist.loader as mnist
+import matplotlib.pylab as plt
+from sklearn.preprocessing import OneHotEncoder
+
+
+class ANN:
+    def __init__(self, layers_size):
+        self.layers_size = layers_size
+        self.parameters = {}
+        self.L = len(self.layers_size)
+        self.n = 0
+        self.costs = []
+
+    def sigmoid(self, Z):
+        return 1 / (1 + np.exp(-Z))
+
+    def softmax(self, Z):
+        expZ = np.exp(Z - np.max(Z))
+        return expZ / expZ.sum(axis=0, keepdims=True)
+
+    def initialize_parameters(self):
+        np.random.seed(1)
+
+        for l in range(1, len(self.layers_size)):
+            self.parameters["W" + str(l)] = np.random.randn(self.layers_size[l], self.layers_size[l - 1]) / np.sqrt(
+                self.layers_size[l - 1])
+            self.parameters["b" + str(l)] = np.zeros((self.layers_size[l], 1))
+
+    def forward(self, X):
+        store = {}
+
+        A = X.T
+        for l in range(self.L - 1):
+            Z = self.parameters["W" + str(l + 1)].dot(A) + self.parameters["b" + str(l + 1)]
+            A = self.sigmoid(Z)
+            store["A" + str(l + 1)] = A
+            store["W" + str(l + 1)] = self.parameters["W" + str(l + 1)]
+            store["Z" + str(l + 1)] = Z
+
+        Z = self.parameters["W" + str(self.L)].dot(A) + self.parameters["b" + str(self.L)]
+        A = self.softmax(Z)
+        store["A" + str(self.L)] = A
+        store["W" + str(self.L)] = self.parameters["W" + str(self.L)]
+        store["Z" + str(self.L)] = Z
+
+        return A, store
+
+    def sigmoid_derivative(self, Z):
+        s = 1 / (1 + np.exp(-Z))
+        return s * (1 - s)
+
+    def backward(self, X, Y, store):
+
+        derivatives = {}
+
+        store["A0"] = X.T
+
+        A = store["A" + str(self.L)]
+        dZ = A - Y.T
+
+        dW = dZ.dot(store["A" + str(self.L - 1)].T) / self.n
+        db = np.sum(dZ, axis=1, keepdims=True) / self.n
+        dAPrev = store["W" + str(self.L)].T.dot(dZ)
+
+        derivatives["dW" + str(self.L)] = dW
+        derivatives["db" + str(self.L)] = db
+
+        for l in range(self.L - 1, 0, -1):
+            dZ = dAPrev * self.sigmoid_derivative(store["Z" + str(l)])
+            dW = 1. / self.n * dZ.dot(store["A" + str(l - 1)].T)
+            db = 1. / self.n * np.sum(dZ, axis=1, keepdims=True)
+            if l > 1:
+                dAPrev = store["W" + str(l)].T.dot(dZ)
+
+            derivatives["dW" + str(l)] = dW
+            derivatives["db" + str(l)] = db
+
+        return derivatives
+
+    def fit(self, X, Y, learning_rate=0.01, n_iterations=2500):
+        np.random.seed(1)
+
+        self.n = X.shape[0]
+
+        self.layers_size.insert(0, X.shape[1])
+
+        self.initialize_parameters()
+        for loop in range(n_iterations):
+            A, store = self.forward(X)
+            cost = -np.mean(Y * np.log(A.T))
+            derivatives = self.backward(X, Y, store)
+
+            for l in range(1, self.L + 1):
+                self.parameters["W" + str(l)] = self.parameters["W" + str(l)] - learning_rate * derivatives[
+                    "dW" + str(l)]
+                self.parameters["b" + str(l)] = self.parameters["b" + str(l)] - learning_rate * derivatives[
+                    "db" + str(l)]
+
+            if loop % 100 == 0:
+                print(cost)
+                # self.predict(X, Y)
+
+            if loop % 10 == 0:
+                self.costs.append(cost)
+
+    def predict(self, X, Y):
+        A, cache = self.forward(X)
+        y_hat = np.argmax(A, axis=0)
+        Y = np.argmax(Y, axis=1)
+        accuracy = (y_hat == Y).mean()
+        print("Accuracy :", accuracy * 100)
+
+    def plot_cost(self):
+        plt.figure()
+        plt.plot(np.arange(len(self.costs)), self.costs)
+        plt.xlabel("epochs")
+        plt.ylabel("cost")
+        plt.show()
+
+
+def pre_process_data(train_x, train_y, test_x, test_y):
+    # Normalize
+    train_x = train_x / 255.
+    test_x = test_x / 255.
+
+    enc = OneHotEncoder(sparse=False, categories='auto')
+    train_y = enc.fit_transform(train_y.reshape(len(train_y), -1))
+
+    test_y = enc.transform(test_y.reshape(len(test_y), -1))
+
+    return train_x, train_y, test_x, test_y
+
+
+if __name__ == '__main__':
+    train_x, train_y, test_x, test_y = mnist.get_data()
+
+    train_x, train_y, test_x, test_y = pre_process_data(train_x, train_y, test_x, test_y)
+
+    print("train_x's shape: " + str(train_x.shape))
+    print("test_x's shape: " + str(test_x.shape))
+
+    layers_dims = [192, 10]
+
+    ann = ANN(layers_dims)
+    ann.fit(train_x, train_y, learning_rate=0.1, n_iterations=10)
+    ann.predict(train_x, train_y)
+    ann.predict(test_x, test_y)
+    ann.plot_cost()
